1096fd6e33b420392a97c80e0599425a8568b8d1
hrisabhy <hrisabhyadav31@gmail.com>
2024-05-18 15:39:51 +0530

    Fetch property data from backend

--- app/components/properties/PropertiesList.tsx
+++ app/components/properties/PropertiesList.tsx
@@ -1,13 +1,40 @@
+"use client";
+
 import PropertiesListItem from "./PropertiesListItem";
+import { useEffect, useState } from "react";
+
+export type PropertyType = {
+  id: string;
+  title: string;
+  price_per_night: number;
+  image_url: string;
+};
 
 const PropertiesList = () => {
+  const [properties, setProperties] = useState<PropertyType[]>([]);
+  const getProperties = async () => {
+    const url = "http://localhost:8000/api/properties";
+    await fetch(url, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log("json", json);
+        setProperties(json.data);
+      })
+      .then((error) => {
+        console.log("error", error);
+      });
+  };
+  useEffect(() => {
+    getProperties();
+    console.log("testing..");
+  }, []);
   return (
     <>
-      <PropertiesListItem />
-      <PropertiesListItem />
-      <PropertiesListItem />
-      <PropertiesListItem />
-      <PropertiesListItem />
+      {properties.map((property) => {
+        return <PropertiesListItem key={property.id} property={property} />;
+      })}
     </>
   );
 };
--- app/components/properties/PropertiesListItem.tsx
+++ app/components/properties/PropertiesListItem.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
+import { PropertyType } from "./PropertiesList";
 
-const PropertiesListItem = () => {
+interface PropertyProps {
+  property: PropertyType;
+}
+const PropertiesListItem: React.FC<PropertyProps> = ({ property }) => {
   return (
     <div className="cursor-pointer p-2">
       <div className="relative overflow-hidden aspect-square rounded-xl">
         <Image
           fill
-          src="/beach-image.jpeg"
+          src={property.image_url}
           sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
           className="hover:scale-110 object-cover transition h-full w-full"
           alt="Beach house"
@@ -14,12 +18,12 @@ const PropertiesListItem = () => {
       </div>
 
       <div className="mt-5">
-        <p className="text-lg font-bold">Property Name</p>
+        <p className="text-lg font-bold">{property.title}</p>
       </div>
 
       <div className="mt-2">
         <p className="text-sm text-gray-500">
-          <strong>$200</strong> per person
+          <strong>${property.price_per_night}</strong> per person
         </p>
       </div>
     </div>
